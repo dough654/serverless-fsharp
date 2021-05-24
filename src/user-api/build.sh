6b38f2a316f5fe3033b94cf425613b2a6ff732fe
Doug <doug@pop-os.localdomain>
2021-05-23 20:11:11 -0400

    Build and deployment verified

--- src/user-api/build.sh
+++ src/user-api/build.sh
@@ -7,6 +7,4 @@ then
   apt -qq -y install zip
 fi
 
-dotnet restore
-dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1
 dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/Release/netcoreapp3.1/deploy-package.zip
